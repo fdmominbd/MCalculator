4bb4c914f26bb59bd2682ba9b78db8fa04d2c420
Emam999 <mdemamshikder1@gmail.com>
2022-01-24 00:09:47 -0600

    1st commit 24.1.22

---
+++ android/app/src/main/kotlin/com/example/mcalculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mcalculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'bmi_store.dart';
+import 'screens/input_page.dart';
+
+void main() => runApp(
+    VxState(
+    store: BMIStore(),
+    interceptors: [LogInterceptor()],
+    child: MCalculator()));
+
+class MCalculator extends StatelessWidget {
+  const MCalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Color(0xFF0F0F1E),
+      ),
+    );
+  }
+}
